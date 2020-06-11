6710d807e6e9d6ed10677149258da5644ef063b8
David <dm.dsgnr@gmail.com>
2020-06-11 12:21:13 +0300

    Подключает React и описывает первые компоненты

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {App} from "./components/app.jsx";
+
+const ROOT_ELEMENT = document.getElementById(`root`);
+
+const settings = {
+  gameTime: 5,
+  errorCount: 3,
+};
+
+ReactDOM.render(
+    <App
+      errorCount = {settings.errorCount}
+      gameTime = {settings.gameTime}
+    />, ROOT_ELEMENT
+);
